cbbce05a93308114dd35e15c961ea515d7ce6a21
Merlinthewizord <142792777+Merlinthewizord@users.noreply.github.com>
2025-11-09 05:25:28 -0600

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -63,8 +63,8 @@ export const DEFAULT_POOL = POOLS[0]
 export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
   {
     mint: FAKE_TOKEN_MINT,
-    name: 'Fake',
-    symbol: 'FAKE',
+    name: 'Trench World v2',
+    symbol: 'TRENCH',
     image: '/fakemoney.png',
     baseWager: 1e9,
     decimals: 9,
